64da1dedbf90c642230e6ce44b3f524e5ed59ec5
David Arroyo <daarroyo19@gmail.com>
2023-11-05 19:58:39 +0100

    Extract&Transform

--- Jupyter Notebooks/funmetar.py
+++ Jupyter Notebooks/funmetar.py
@@ -5,11 +5,7 @@ import pandas as pd
 import re
 def check_nan(df: pd.DataFrame) -> None:
     
-    """
-    Recibe un dataframe y enseña el % de valores nulos
-    y lo grafica
-    """
-    
+
     nan_cols = df.isna().mean() * 100  # porcentaje de nulo en cada columna
     
     display(f'N nan cols: {len(nan_cols[nan_cols>0])}')
@@ -27,19 +23,34 @@ def check_nan(df: pd.DataFrame) -> None:
                )
 
     plt.show();
-
-def generar_columna_gust(valor):
-    pattern = re.compile(r'^(\d{2})\s')  # Dos dígitos seguidos de un espacio
-    match = pattern.match(valor)
-
-
-    if match:
-        return match.group(1)
-
-    else:
-        return 0
-
 def fix_hum_column(row):
+    """
+    Corrige la columna 'Relative_hum' en un DataFrame.
+    
+    Se examina el valor de 'Relative_hum' en una fila dada. Si el valor está marcado como '>', se intenta extraer 
+    el porcentaje de humedad de la columna 'Pressure' y devuelve el valor encontrado. En caso de no hallar el valor, 
+    se mantiene 'Relative_hum' sin cambios.
+    
+    Parameters
+    ----------
+    row : pandas.Series
+        Fila de un DataFrame. Se asume que contiene al menos las columnas 'Relative_hum' y 'Pressure'.
+
+    Returns
+    -------
+    str
+        Si 'Relative_hum' contiene '>', se intenta extraer el porcentaje de humedad de 'Pressure'. Si se encuentra, 
+        se devuelve el valor. En caso contrario, se devuelve 'Relative_hum' sin modificar.
+
+    Notes
+    -----
+    Se asume que 'Pressure' contiene una cadena que incluye el valor de humedad (expresado en porcentaje) para 
+    'Relative_hum' cuando 'Relative_hum' tiene el valor '>'. El valor esperado está en formato de dos caracteres 
+    seguidos por el símbolo '%' (por ejemplo, '85%'). Si no se encuentra ningún valor, se mantiene 'Relative_hum' 
+    sin cambios.
+
+    """
+    
     if row['Relative_hum'].strip() == '>':
         match = re.search(r'(\w{2})%', row['Pressure'])
         
@@ -49,4 +60,33 @@ def fix_hum_column(row):
             return row['Relative_hum']
     else:
         return row['Relative_hum']
+
+
+def generar_columna_gust(valor):
+    """
+    Extrae los dos primeros dígitos seguidos de un espacio del valor de la columna pressure
+    ya que erroneamente los valores de ráfagas(gusts) cuando los hay se encuentran es esta columna.
+    
+    Parameters
+    ----------
+    valor : str
+        Cadena de entrada desde la cual se intentan extraer los dos primeros dígitos.
+
+    Returns
+    -------
+    str or int
+        Si se encuentran dos dígitos seguidos de un espacio al principio de 'valor', se devuelve 
+        la cadena que representa esos dos dígitos. De lo contrario, se devuelve 0.
+        
+    """
+ 
+    pattern = re.compile(r'^(\d{2})\s')  # Dos dígitos seguidos de un espacio
+    match = pattern.match(valor)
+
+    if match:
+        return match.group(1)
+
+    else:
+        return 0 
+
 
